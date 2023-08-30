b232ae821b7abd360a4026b80076ea91ad24a186
GraceVC <84336595+GraceVC@users.noreply.github.com>
2023-08-30 08:10:45 -0500

    App Actualizada

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,36 @@ import { PetForm } from './components/PetForm';
 import { useDatePet } from './hooks/useDatePet'
 
 function App() {
-
+  // Desestructura las funciones proporcionadas por el hook useDatePet para su uso 
   const { registerPet, listPet, editDate, setEditDate, deleteDatePet, editDatePet } = useDatePet();
 
   return (
     <div className="container mx-auto">
+      {/* Componente de encabezado */}
       <Header />
-      <div className=" flex flex-col sm:flex-row">
-        <div className="ml-8 mr-3 sm:w-1/2 ">
-          <PetForm registerPet={registerPet} editDate={editDate} editDatePet={editDatePet} setEditDate={setEditDate} />
+
+      {/* Contenedor principal con disposición de columnas en dispositivos pequeños */}
+      <div className="flex flex-col sm:flex-row">
+
+        {/* Formulario de registro de mascotas */}
+        <div className="ml-8 mr-3 sm:w-1/2">
+          <PetForm
+            registerPet={registerPet}
+            editDate={editDate}
+            editDatePet={editDatePet}
+            setEditDate={setEditDate}
+          />
         </div>
-        <div className="ml-8 mr-3 sm:w-1/2 ">
-          <Patient listPet={listPet} setEditDate={setEditDate} deleteDatePet={deleteDatePet} />
+
+        {/* Lista de pacientes o mascotas registradas */}
+        <div className="ml-8 mr-3 sm:w-1/2">
+          <Patient
+            listPet={listPet}
+            setEditDate={setEditDate}
+            deleteDatePet={deleteDatePet}
+          />
         </div>
+
       </div>
     </div>
   )
