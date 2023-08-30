9b0d6b659e25e0621b222019b931b0a7e180e2a4
GraceVC <84336595+GraceVC@users.noreply.github.com>
2023-08-30 16:23:09 -0500

    AdminPacientes_Actualizada

--- src/citas/CitaForm.jsx
+++ src/citas/CitaForm.jsx
@@ -2,20 +2,22 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function CitaForm() {
+  // Estados para almacenar los datos del formulario
   const [fecha, setFecha] = useState('');
   const [hora, setHora] = useState('');
   const [mascota, setMascota] = useState('');
   const [propietario, setPropietario] = useState('');
   const [motivo, setMotivo] = useState('');
 
+  // Función para manejar el envío del formulario
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
+      // Enviar los datos a través de una solicitud POST a la API
       await axios.post('/api/citas', {
         fecha, hora, mascota, propietario, motivo
       });
-      // Actualizar la lista de citas o realizar alguna otra acción necesaria
     } catch (error) {
       console.error(error);
     }
@@ -26,6 +28,55 @@ function CitaForm() {
       <h2>Crear Cita</h2>
       <form onSubmit={handleSubmit}>
         {/* Campos de formulario */}
+        <div>
+          <label htmlFor="fecha">Fecha:</label>
+          <input
+            type="date"
+            id="fecha"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="hora">Hora:</label>
+          <input
+            type="time"
+            id="hora"
+            value={hora}
+            onChange={(e) => setHora(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="mascota">Mascota:</label>
+          <input
+            type="text"
+            id="mascota"
+            value={mascota}
+            onChange={(e) => setMascota(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="propietario">Propietario:</label>
+          <input
+            type="text"
+            id="propietario"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="motivo">Motivo:</label>
+          <textarea
+            id="motivo"
+            value={motivo}
+            onChange={(e) => setMotivo(e.target.value)}
+            required
+          />
+        </div>
         <button type="submit">Crear Cita</button>
       </form>
     </div>
--- src/components/Patient.tsx
+++ src/components/Patient.tsx
@@ -1,16 +1,22 @@
+// Importando la interfaz "Pet" del archivo "../types"
 import { Pet } from "../types";
+
+// Importando el componente "PetList" del archivo "./PetList"
 import { PetList } from "./PetList";
 
+// Definición de la interfaz "Props" que describe las propiedades que recibe el componente "Patient"
 interface Props {
-  listPet: Pet[];
-  setEditDate: (pet: Pet) => void;
-  deleteDatePet: (id: string) => void;
+  listPet: Pet[]; // Lista de objetos de tipo "Pet"
+  setEditDate: (pet: Pet) => void; // Función para editar una cita
+  deleteDatePet: (id: string) => void; // Función para eliminar una cita por su ID
 }
 
+// Definición del componente "Patient"
 export const Patient = ({ listPet, setEditDate, deleteDatePet }: Props) => {
   return (
     <div className="md:h-screen overflow-y-scroll">
       {listPet.length ? (
+        // Si hay elementos en la lista de pacientes (listPet)
         <>
           <h2 className="font-black text-3xl  text-center">
             Listado de pacientes
@@ -19,11 +25,13 @@ export const Patient = ({ listPet, setEditDate, deleteDatePet }: Props) => {
             Administra tus{" "}
             <span className="text-zinc-600 font-bold "> pacientes y citas</span>
           </p>
+          {/* Mapeo de la lista de pacientes para generar componentes "PetList" */}
           {listPet.map(date => (
             <PetList date={date} key={date.id} setEditDate={setEditDate} deleteDatePet={deleteDatePet} />
           ))}
         </>
       ) : (
+        // Si la lista de pacientes está vacía
         <>
           <h2 className="font-black text-3xl  text-center">
             Listado de pacientes
@@ -40,3 +48,4 @@ export const Patient = ({ listPet, setEditDate, deleteDatePet }: Props) => {
     </div>
   );
 };
+
